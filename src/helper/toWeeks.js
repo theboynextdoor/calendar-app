74b239c45dc994809367a4e0c5d1317b91373f5f
theboynextdoor <dwhywood@gmail.com>
2018-10-26 17:26:58 +0000

    Refactor toWeeks function

--- src/helper/toWeeks.js
+++ src/helper/toWeeks.js
@@ -1,20 +1,24 @@
 import isSameWeek from 'date-fns/is_same_week'; 
+import lastDayOfWeek from 'date-fns/last_day_of_week';
+import isEqual from 'date-fns/is_equal';
 
 function toWeeks(days) {
     let weeks = [];
-    let nth = 0;
+    let week = []; 
+    let currentWeek = lastDayOfWeek(days[0]); 
     
-    for (let current = 0; current < days.length; current++) {
-        if (current === 0) {
-            weeks[nth] = [ days[current] ]; 
-        } else if(isSameWeek(days[current], days[current - 1])) {
-            weeks[nth].push(days[current]);
+    for (let nth = 0; nth < days.length; nth++) {
+        if (isEqual(currentWeek, lastDayOfWeek(days[nth]))) {
+            week.push(days[nth]); 
         } else {
-            weeks[++nth] = [ days[current] ] 
+            weeks.push(week); 
+            week = [];
+            week.push(days[nth]);
+            currentWeek = lastDayOfWeek(days[nth])
         }
     }
     
-    return weeks; 
+    return weeks;
 }
 
 export default toWeeks; 
