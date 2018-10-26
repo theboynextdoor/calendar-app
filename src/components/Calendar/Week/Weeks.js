62f425beb83ab1cb743dfed2d5caed3a559e80b3
theboynextdoor <dwhywood@gmail.com>
2018-10-26 18:20:07 +0000

    Create week component

---
+++ src/components/Calendar/Week/Weeks.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import lastDayOfWeek from 'date-fns/last_day_of_week';
+
+function Weeks(props) {
+    let reminders = props.reminders, 
+        days = props.days; 
+        
+    
+}
--- src/components/Calendar/Week/index.js
+++ src/components/Calendar/Week/index.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import Day from '../Day';
-import './style.css';
+import React from "react";
+import Day from "../Day";
+import Reminder from "../../Reminder";
+import getDate from "date-fns/get_date";
+import getDay from "date-fns/get_day";
+import "./style.css";
 
-
-function Week(props) {
-    return (
-        <div className='calendar-week'>
-            {props.children}
-        </div>
-    );
+function _styleDay(num) {
+  return { 
+    marginLeft: num * 14.2857143 + "%",
+    borderLeft: "1px solid #e0e0e0"
+  }
 }
 
-
+function Week(props) {
+  let week = Object.keys(props.days).map((day, index) => {
+              return (
+                <Day 
+                  key={day} 
+                  day={getDate(day)} 
+                  style={(index === 0) ? _styleDay(getDay(day)): {}}>
+                  
+                  {props.day.reminders.map((reminder) => (<Reminder reminder={props.reminders[reminder]} />))}
+                  
+                </Day>
+              );
+            });
+            
+  return (
+    <div className="calendar__week">
+      {week}
+    </div>
+  );
+}
 
 export default Week; 
